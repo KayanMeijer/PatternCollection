d3a44760b5218976c7615fa4e4a2d7838e59e6ae
Kayan Meijer <k.meijer1@live.nl>
2016-01-11 20:55:40 +0100

    Added Dependency Injection.

--- PatternCollection.DependencyInjection/DependencyInjection.cs
+++ PatternCollection.DependencyInjection/DependencyInjection.cs
@@ -1,10 +1,16 @@
-﻿namespace PatternCollection.DependencyInjection
+﻿using PatternCollection.DependencyInjection.Models;
+using Spring.Context.Support;
+
+namespace PatternCollection.DependencyInjection
 {
     public class DependencyInjection
     {
         public static void Main(string[] args)
         {
+            var context = ContextRegistry.GetContext();
+            var instance = context.GetObject<DependencyInjectionExample>("DependencyInjectionExample");
 
+            instance.Run();
         }
     }
 }
---
+++ PatternCollection.DependencyInjection/Models/DependencyInjectionExample.cs
@@ -0,0 +1,12 @@
+﻿namespace PatternCollection.DependencyInjection.Models
+{
+    public class DependencyInjectionExample
+    {
+        public ApiConfig ApiConfig { get; private set; }
+
+        public void Run()
+        {
+            ApiConfig.Print();
+        }
+    }
+}
