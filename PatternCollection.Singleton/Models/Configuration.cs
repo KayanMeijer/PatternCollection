a6ec0edf75d2691aad728fe5ec10898f7c2682ac
Kayan Meijer <k.meijer1@live.nl>
2016-01-11 13:04:17 +0100

    Updated singleton pattern.

---
+++ PatternCollection.Singleton/Models/Configuration.cs
@@ -0,0 +1,21 @@
+﻿namespace PatternCollection.Singleton.Models
+{
+    public class Configuration
+    {
+        private static Configuration currentInstance;
+
+        public string BaseUrl { get; set; }
+        public int DefaultPort { get; set; }
+
+        public static Configuration Instance
+        {
+            get
+            {
+                if (currentInstance == null)
+                    currentInstance = new Configuration();
+
+                return currentInstance;
+            }
+        }
+    }
+}
--- PatternCollection.Singleton/Program.cs
+++ PatternCollection.Singleton/Program.cs
@@ -1,15 +1,20 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using PatternCollection.Singleton.Models;
 
 namespace PatternCollection.Singleton
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
+            var config = Configuration.Instance;
+
+            config.BaseUrl = "api.itwasntme007.nl";
+            config.DefaultPort = 80;
+
+            Console.WriteLine("BaseUrl: " + config.BaseUrl);
+            Console.WriteLine("DefaultPort: " + config.DefaultPort);
+            Console.ReadLine();
         }
     }
 }
